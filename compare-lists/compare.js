c520c3254d28f141b03b7750378891c18b0d218f
Kory Hoopes <kory@hey.com>
2022-02-02 21:38:20 -0700

    Source formatting
    Cleaning up the code so it's a bit easier to read.

--- compare-lists/compare.js
+++ compare-lists/compare.js
@@ -1,33 +1,36 @@
-const boyNames2000 = require('./boy-names-2000').sort();
-const boyNames2020 = require('./boy-names-2020').sort();
+const boyNames = {
+    '2000': require('./boy-names-2000').sort(),
+    '2020': require('./boy-names-2020').sort()
+};
 
-const boyNames2000Delta = compare(boyNames2000, boyNames2020, true);
-const boyNames2020Delta = compare(boyNames2020, boyNames2000, true);
-const overlap = compare(boyNames2000, boyNames2020);
+const boyNamesDelta = {
+    '2000': compare(boyNames[2000], boyNames[2020], true),
+    '2020': compare(boyNames[2020], boyNames[2000], true),
+    'overlap': compare(boyNames[2000], boyNames[2020])
+};
 
 function compare(array1, array2, delta) {
-    const
-        findItem = (item1) => array2.find(item2 => item2 === item1),
-        findDelta = (item1) => delta ? !findItem(item1) : findItem(item1);
+    const findDelta = (item1) => delta ? !findItem(item1) : findItem(item1);
+    const findItem = (item1) => array2.find(item2 => item2 === item1);
 
     return array1.filter(item1 => findDelta(item1));
 }
 
 console.log(
 `
-Boy Names Year 2000 (${boyNames2000.length}):
-${boyNames2000}
+Boy Names Year 2000 (${boyNames[2000].length}):
+${boyNames[2000]}
 
-Boy Names Year 2000 Delta (${boyNames2000Delta.length}):
-${boyNames2000Delta}
+Boy Names Year 2000 Delta (${boyNamesDelta[2000].length}):
+${boyNamesDelta[2000]}
 
-Boy Names Year 2020  (${boyNames2020.length}):
-${boyNames2020}
+Boy Names Year 2020  (${boyNames[2020].length}):
+${boyNames[2020]}
 
-Boy Names Year 2020 Delta (${boyNames2020Delta.length}):
-${boyNames2020Delta}
+Boy Names Year 2020 Delta (${boyNamesDelta[2020].length}):
+${boyNamesDelta[2020]}
 
-Boy Names in both Years (${overlap.length}):
-${overlap}
+Boy Names in Both Years (${boyNamesDelta.overlap.length}):
+${boyNamesDelta.overlap}
 `
 );
