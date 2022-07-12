81d8ac598c710b34f853e6496d7b6488bf516244
Kory Hoopes <kory@hey.com>
2022-07-12 13:38:42 -0600

    Moving stuff around...
    Trying to stay organized, is it worth it?

--- swift/lets_play.playground.playground/Contents.swift
+++ swift/lets_play.playground.playground/Contents.swift
@@ -1,16 +1,2 @@
 import Foundation
 
-var array = [String]()
-var startTime: Double
-var endTime: Double
-var totalTime: Double
-
-startTime = Date.now.timeIntervalSince1970 * 1000
-for i in 1...10000 {
-    array.append("Line \(i)")
-}
-endTime = Date.now.timeIntervalSince1970 * 1000
-
-totalTime = endTime - startTime
-
-print("Total Run Time: \(totalTime) milliseconds.")
--- templates/nodejs-typescript/src/app/index.ts
+++
@@ -1,5 +0,0 @@
-const sanityCheck = 'Running TypeScript on Node.js';
-
-console.log(sanityCheck);
-
-export { sanityCheck };
