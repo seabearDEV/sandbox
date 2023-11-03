d687a6ae5ec3e86ca81538e2e853129d5870e227
Kory Hoopes <kory@hey.com>
2023-11-03 14:03:53 -0600

    Static content directory is now working
    Learning how to get a "__dirname" variable in NodeJS while using modules, shouldn't be this complicated.

--- simple-genesis/src/app/index.ts
+++ simple-genesis/src/app/index.ts
@@ -1,5 +1,22 @@
-const index = "Your project-genesis application is running...";
+// Module imports
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 
-console.log(index);
+// Path configuration
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-export { index };
+// Web server configuration
+const web = {
+    port: 4242,
+    server: express()
+};
+
+// Directory to serve static content from
+web.server.use(express.static(path.join(__dirname, '../public')));
+
+// Start the web server
+web.server.listen(web.port, () => {
+    console.log(`Your website is listening on port ${web.port}...`);
+});
