046f8304eab316942c866bbad05d98d836e6f0f5
Kory Hoopes <kory@hey.com>
2023-11-10 16:27:31 -0700

    Changing to Fastify for routing
    Removing Express and adding Fastify for routing.

--- simple-genesis/src/app/index.ts
+++ simple-genesis/src/app/index.ts
@@ -1,22 +1,37 @@
-// Module imports
-import express from 'express';
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
+import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify'
 
-// Path configuration
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const server: FastifyInstance = Fastify({})
 
-// Web server configuration
-const web = {
-    port: 4242,
-    server: express()
-};
+const opts: RouteShorthandOptions = {
+    schema: {
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    pong: {
+                        type: 'string'
+                    }
+                }
+            }
+        }
+    }
+}
 
-// Directory to serve static content from
-web.server.use(express.static(path.join(__dirname, '../public')));
+server.get('/ping', opts, async (request, reply) => {
+    return { pong: 'it worked!' }
+})
 
-// Start the web server
-web.server.listen(web.port, () => {
-    console.log(`Your website is listening on port ${web.port}...`);
-});
+const start = async () => {
+    try {
+        await server.listen({ port: 3000 })
+
+        const address = server.server.address()
+        const port = typeof address === 'string' ? address : address?.port
+
+    } catch (err) {
+        server.log.error(err)
+        process.exit(1)
+    }
+}
+
+start()
