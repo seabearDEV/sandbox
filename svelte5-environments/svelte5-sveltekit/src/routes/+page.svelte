bc53faea7f7d37a41e7aed83ff6d103435b6cb22
Kory Hoopes <kory.hoopes@broadcom.com>
2025-02-21 15:12:27 -0700

    Trying out Vite vs SvelteKit for developing static pages.

---
+++ svelte5-environments/svelte5-sveltekit/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<!-- TypeScript-->
+<script lang="ts">
+	// Svelte Imports
+	import { onMount } from 'svelte';
+
+	// Component Imports
+	import Header from './header.svelte';
+
+	// Component Variables
+	let mounted = false;
+
+	onMount(() => {
+		mounted = true;
+	});
+</script>
+
+<!-- Content -->
+<Header />
+
